080f7655195ee44a405bdfab573cd85e16b0cb09
Devin Fischer <devinfischer32@gmail.com>
2021-08-22 23:37:14 -0600

    Finished

--- server/index.js
+++ server/index.js
@@ -8,17 +8,12 @@ app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
 app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+  const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
 
-  // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
 
-  res.status(200).send(randomCompliment);
-  
+  res.status(200).send(randomCompliment); 
 });
 
 app.get('/api/fortune',(req,res)=>{
@@ -32,31 +27,12 @@ app.get('/api/fortune',(req,res)=>{
 
 app.get('/api/lucky',(req,res)=>{
   let luckyCont = []
-  let randomNumbers = Math.floor(Math.random()* 777) + 1 
+  let randomNumbers = Math.floor(Math.random()* 7) + 1 
   luckyCont.push(randomNumbers)
 
   res.status(200).send(luckyCont)
 });
 
-//I wasted too much time trying to get other things to work didn't even try to finish this part
-app.get("/api/people/?search=r2", (req, res) => {
-  
-  res.status(200).send(rd)
-
-})
-
 
 app.listen(4000, () => console.log("Server running on 4000"));
 
-// app.post('/api/users/',(req,res)=>{
-  //   let users = []
-  //   const {name} = req.body
-  //   const {email} = req.body
-  //   let userObj = {
-    //     name: name.value,
-    //     email: email.value
-    //   }
-    //   users.push(userObj)
-    //   console.log(userObj)
-    //   res.status(200)
-    // })
